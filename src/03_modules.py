ab0bf87f18ebdaf600c2b6f88b3f2b14e8aeb99c
Nate Dow <ndow33@gmail.com>
2020-08-31 16:45:05 -0600

    Monday the 31st of August

--- src/03_modules.py
+++ src/03_modules.py
@@ -8,24 +8,31 @@ level operating system functionality.
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
+
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print('_________________________________________________')
+for command in dir(sys.argv):
+    print(command)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print('_________________________________________________')
+print(sys.platform)
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
-
+print('_________________________________________________')
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print('_________________________________________________')
+print(os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print('_________________________________________________')
+print(os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print('_________________________________________________')
+print(os.getlogin())
